c0175af1e10e4c1b30dce8c01ca0ca07f90d461e
“x <2676697536@qq.com>
2021-01-21 14:03:12 +0800

    上传项目

---
+++ src/pages/Posts1.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Post1</h1>
+      <ul v-for="edge in $page.posts.edges" :key="edge.node.id">
+        <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        id
+        title
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: "Post1",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Post Page</h1>
+      <h2>{{ $page.post.title }}</h2>
+      <div>{{ $page.post.content }}</div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!){
+  post (id: $id){
+    id
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: "PostPage",
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
